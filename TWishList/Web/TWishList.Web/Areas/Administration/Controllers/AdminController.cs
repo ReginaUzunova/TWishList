53bdb90dc75d9a980c1100fe98029ae5440f0a1b
ReginaUzunova <reginatrn@gmail.com>
2019-07-30 17:38:59 +0300

    Added Administration area

---
+++ TWishList/Web/TWishList.Web/Areas/Administration/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿namespace TWishList.Web.Areas.Administration.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using TWishList.Common;
+    using TWishList.Web.Controllers;
+
+    [Area("Administration")]
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    public class AdminController : BaseController
+    {
+    }
+}
---
+++ TWishList/Web/TWishList.Web/Areas/Administration/Controllers/OfferController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TWishList.Web.Areas.Administration.Controllers
+{
+    public class OfferController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
