c3046f91bd54742a98ff894cc75573b8cf8cc42a
ReginaUzunova <reginatrn@gmail.com>
2019-08-01 14:32:04 +0300

    Added BaseModel and BaseDeletableModel

---
+++ TWishList/Data/TWishList.Data.Common/Models/IDeletableEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TWishList.Data.Common.Models
+{
+    class IDeletableEntity
+    {
+    }
+}
--- TWishList/Data/TWishList.Data.Models/Country.cs
+++ TWishList/Data/TWishList.Data.Models/Country.cs
@@ -2,8 +2,9 @@
 {
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using TWishList.Data.Common.Models;
 
-    public class Country
+    public class Country : BaseDeletableModel
     {
         public Country()
         {
