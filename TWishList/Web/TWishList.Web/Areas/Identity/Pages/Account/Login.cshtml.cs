e2f4f38cee2b85792dbf79376008112ee41c9adb
ReginaUzunova <reginatrn@gmail.com>
2019-08-06 16:17:16 +0300

    Modified Login and Register views

--- TWishList/Web/TWishList.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ TWishList/Web/TWishList.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -70,9 +70,8 @@
 
             if (ModelState.IsValid)
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
